9bc5ce6d4bd26216133b8677408ddcd07dd5529b
Thet Naing <thet.naing@moneysupermarket.com>
2017-09-19 13:33:37 +0100

    testing multiple files resource

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -2,6 +2,25 @@ class profile::base {
   file { '/tmp/profiletest' :
     ensure  => 'present',
     content => 'puppet test is good',
+    owner => 'vagrant',
+  }
+
+  file { '/tmp/profiletest1' :
+    ensure  => 'present',
+    content => 'puppet test is good',
+    owner => 'vagrant',
+  }
+
+  File {
+    owner => 'root',
+    mode  => '0755',
+  }
+  
+  file { '/tmp/test1.service' :
+    source => 'puppet:///modules/profile/message',
+  }
+  file { '/tmp/test2.service' :
+    source => 'puppet:///modules/profile/message',
   }
 
   include ::timezone
